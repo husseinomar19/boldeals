24fe29cf117e7ffef4fe6b8c589806012e20e818
Hussein <omerhussen603@gmail.com>
2025-03-25 16:40:29 +0100

    first commit

---
+++ components/navbar.tsx
@@ -0,0 +1,74 @@
+import {
+  Navbar as HeroUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+} from "@heroui/navbar";
+import { Input } from "@heroui/input";
+import NextLink from "next/link";
+import { ThemeSwitch } from "@/components/theme-switch";
+import {
+  SearchIcon,
+  Logo,
+} from "@/components/icons";
+
+export const Navbar = () => {
+  const searchInput = (
+    <Input
+      aria-label="Search"
+      classNames={{
+        inputWrapper: "bg-default-100",
+        input: "text-sm",
+      }}
+      
+      labelPlacement="outside"
+      placeholder="Search..."
+      startContent={
+        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+      }
+      type="search"
+    />
+  );
+
+  return (
+    <HeroUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <Logo />
+            <p className="font-bold text-inherit">BolDeals</p>
+          </NextLink>
+
+        </NavbarBrand>
+
+
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+        <ThemeSwitch />
+
+      </NavbarItem>
+        <NavbarItem className="hidden lg:flex md:flex">{searchInput}</NavbarItem>
+        <NavbarItem className="hidden md:flex">
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <ThemeSwitch />
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarMenu>
+        {searchInput}
+      </NavbarMenu>
+
+    </HeroUINavbar>
+  );
+};
