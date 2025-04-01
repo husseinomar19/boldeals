d5347267bcbc1d7e8c27fb12b987ab980b2780a9
Hussein <omerhussen603@gmail.com>
2025-04-01 12:13:29 +0200

    remove site description from configuration

--- config/site.ts
+++ config/site.ts
@@ -2,5 +2,5 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "boldeals",
-  description: "Make beautiful websites regardless of your design experience.",
+  description: "",
 };
